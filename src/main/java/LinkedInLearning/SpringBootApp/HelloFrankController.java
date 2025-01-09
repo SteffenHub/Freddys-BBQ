90c0fdcf8922b2f04e8d5e6343c26a7a407b6b8f
SteffenHub <steffenhub@users.noreply.github.com>
2025-01-09 02:56:36 +0100

    2_2 Ihr erstes Projekt mit dem Spring Initializr

---
+++ src/main/java/LinkedInLearning/SpringBootApp/HelloFrankController.java
@@ -0,0 +1,13 @@
+package LinkedInLearning.SpringBootApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloFrankController {
+    
+    @GetMapping("/")
+    public String get(){
+        return "America, I'm only getting started.";
+    }
+}
