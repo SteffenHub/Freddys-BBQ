9fad401318fff07dbb2040582639f6d3a86d70bc
SteffenHub <steffenhub@users.noreply.github.com>
2025-01-11 00:54:20 +0100

    4_3 Unit-Test des REST-Services mit Mock MVC

--- src/main/java/LinkedInLearning/SpringBootApp/DirtySecretsRestController.java
+++ src/main/java/LinkedInLearning/SpringBootApp/DirtySecretsRestController.java
@@ -2,6 +2,11 @@ package LinkedInLearning.SpringBootApp;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.UUID;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/dirty-secrets")
 public class DirtySecretsRestController {
     
+    
+    private Map<String, DirtySecret> secrets = new TreeMap<>();
+    /* 
     private DirtySecretsRepository repository;
 
     public DirtySecretsRestController(DirtySecretsRepository repository){
@@ -35,6 +43,18 @@ public class DirtySecretsRestController {
     @PostMapping
     public DirtySecret post(@RequestBody DirtySecret secret){
         return this.repository.save(secret);
+    }
+    */
+    @PostMapping
+    public DirtySecret post(@RequestBody DirtySecret secret) {
+        // Id generieren
+        secret.setId(UUID.randomUUID().toString());
+
+        // Secret merken
+        this.secrets.put(secret.getId(), secret);
+
+        // Secret mit Id zurück geben
+        return secret;
     }
 
 }
---
+++ src/test/java/LinkedInLearning/SpringBootApp/DirtySecretsRestControllerTests.java
@@ -0,0 +1,28 @@
+package LinkedInLearning.SpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(DirtySecretsRestController.class)
+public class DirtySecretsRestControllerTests {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void shouldSaveSecrets() throws Exception {
+    // Controller aufrufen
+    this.mockMvc
+        .perform(MockMvcRequestBuilders.post("/dirty-secrets")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{ \"name\": \"Linda\", \"secret\": \"Cheated her son into Stanford.\" }"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNotEmpty());
+  }
+
+}
