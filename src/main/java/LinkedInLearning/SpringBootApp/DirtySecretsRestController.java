739b847dc7e96d649b895eca875e089625fcf51f
SteffenHub <steffenhub@users.noreply.github.com>
2025-01-11 01:20:34 +0100

    4_3 test mit vorhandenem Controller

--- src/main/java/LinkedInLearning/SpringBootApp/DirtySecretsRestController.java
+++ src/main/java/LinkedInLearning/SpringBootApp/DirtySecretsRestController.java
@@ -4,24 +4,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
-import java.util.TreeMap;
-import java.util.UUID;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
 @RestController
 @RequestMapping("/dirty-secrets")
 public class DirtySecretsRestController {
-    
-    
-    private Map<String, DirtySecret> secrets = new TreeMap<>();
-    /* 
+
     private DirtySecretsRepository repository;
 
     public DirtySecretsRestController(DirtySecretsRepository repository){
@@ -43,18 +37,6 @@ public class DirtySecretsRestController {
     @PostMapping
     public DirtySecret post(@RequestBody DirtySecret secret){
         return this.repository.save(secret);
-    }
-    */
-    @PostMapping
-    public DirtySecret post(@RequestBody DirtySecret secret) {
-        // Id generieren
-        secret.setId(UUID.randomUUID().toString());
-
-        // Secret merken
-        this.secrets.put(secret.getId(), secret);
-
-        // Secret mit Id zurück geben
-        return secret;
     }
 
 }
--- src/test/java/LinkedInLearning/SpringBootApp/DirtySecretsRestControllerTests.java
+++ src/test/java/LinkedInLearning/SpringBootApp/DirtySecretsRestControllerTests.java
@@ -1,14 +1,20 @@
 package LinkedInLearning.SpringBootApp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@WebMvcTest(DirtySecretsRestController.class)
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class DirtySecretsRestControllerTests {
 
   @Autowired
