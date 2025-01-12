f527407b43d270610f9e8dad1361313835b32145
SteffenHub <steffenhub@users.noreply.github.com>
2025-01-13 00:28:55 +0100

    6_4 Greetings from properties Konfiguration von YAML bis Umgebungsvariable

---
+++ src/main/java/LinkedInLearning/SpringBootApp/GreetingRestController.java
@@ -0,0 +1,21 @@
+package LinkedInLearning.SpringBootApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingRestController {
+  
+  @Value("${linkedin.greeting}")
+  private String greeting;
+
+  @GetMapping
+  public String get(){
+    return this.greeting;
+  }
+}
