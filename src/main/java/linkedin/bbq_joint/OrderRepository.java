d08e2d397c5ef3452dd7d2f5a78c31263b88bf60
SteffenHub <steffenhub@users.noreply.github.com>
2025-01-25 18:55:44 +0100

    Delete all files

--- src/main/java/linkedin/bbq_joint/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package linkedin.bbq_joint;
-
-import java.util.UUID;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order, UUID> {
-
-}
