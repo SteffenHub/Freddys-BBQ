99ae12813062e9730d3f447d01ef342244ad94f8
SteffenHub <steffenhub@users.noreply.github.com>
2025-01-25 22:15:58 +0100

    2_3 Mit OpenAPI REST-Services dokumentieren

--- order/src/main/java/bbq/order/MenuRestController.java
+++ order/src/main/java/bbq/order/MenuRestController.java
@@ -2,13 +2,21 @@ package bbq.order;
 
 import bbq.order.model.MenuCategory;
 import bbq.order.model.MenuItem;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@Tag(name = "Menu", description = "Menu Resource")
 @RestController
 @RequestMapping("/api/menu")
 @RequiredArgsConstructor
@@ -16,16 +24,24 @@ public class MenuRestController {
 
     private final MenuRepository menuRepository;
 
+    @Operation(summary = "Get menu", description = "The full menu with all categories and items.")
     @GetMapping
     public List<MenuCategory> get() {
         return menuRepository.findAll();
     }
 
+    @Operation(summary = "Get Items by Category", description = "All menu items in a specific category.")
+    @ApiResponse(
+            responseCode = "500",
+            description = "Something went seriously wrong.",
+            content = {@Content(schema = @Schema(implementation = ProblemDetail.class))}
+    )
     @GetMapping("/{key}/menu-items")
     public List<MenuItem> getByKey(@PathVariable String key, @RequestParam Optional<String> sort) {
         return menuRepository.findItemsByCategoryKey(key, sort);
     }
 
+    @Hidden
     @GetMapping("/response-entity")
     public ResponseEntity<List<MenuCategory>> getResponseEntity() {
         var menuCategories = menuRepository.findAll();
