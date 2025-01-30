c3e4662a848ec420db31b071b85992bd2d8f8746
SteffenHub <steffenhub@users.noreply.github.com>
2025-01-30 22:25:56 +0100

    Test Menu Controller

--- src/main/java/linkedin/bbq_joint/MenuController.java
+++ src/main/java/linkedin/bbq_joint/MenuController.java
@@ -1,6 +1,5 @@
 package linkedin.bbq_joint;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +9,12 @@ import org.springframework.ui.Model;
 @Controller
 @RequestMapping("/")
 public class MenuController {
-  
-  @Autowired
-  private MenuItemRepository menuItemRepository;
+
+  private final MenuItemRepository menuItemRepository;
+
+  public MenuController(MenuItemRepository menuItemRepository) {
+    this.menuItemRepository = menuItemRepository;
+  }
 
   @GetMapping
   public String index(Model model){
@@ -20,6 +22,4 @@ public class MenuController {
     model.addAttribute("menuItems", menu);
     return "index";
   }
-  
-
 }
---
+++ src/test/java/linkedin/bbq_joint/MenuControllerTest.java
@@ -0,0 +1,49 @@
+package linkedin.bbq_joint;
+
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@WebMvcTest(MenuController.class)
+class MenuControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private MenuItemRepository menuItemRepository;
+
+    private final List<MenuItem> menuItems = Arrays.asList(
+            new MenuItem(UUID.fromString("00000000-0000-0000-0000-000000000001"), "Freddy's Ribs Special", 20, false),
+            new MenuItem(UUID.fromString("00000000-0000-0000-0000-000000000002"), "Freddy's Lemonade", 5, true),
+            new MenuItem(UUID.fromString("00000000-0000-0000-0000-000000000003"), "Coleslaw Salad", 3, false)
+            );
+
+    @BeforeEach
+    void setUp() {
+        when(menuItemRepository.findByOrderByDrinkDescNameDesc()).thenReturn(this.menuItems);
+    }
+
+    @Test
+    void testIndexEndpoint() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())  // HTTP 200 OK
+                .andExpect(view().name("index"))  // View-Name ist "index"
+                .andExpect(model().attributeExists("menuItems"))  // "menuItems" ist im Model vorhanden
+                .andExpect(model().attribute("menuItems", this.menuItems));  // Vergleicht den Inhalt
+    }
+}
