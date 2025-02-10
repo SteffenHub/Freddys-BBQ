bf726d85c6d93f85422651c9dc0b039f5568a04e
SteffenHub <steffenhub@users.noreply.github.com>
2025-02-10 17:53:39 +0100

    3_2 publish

--- order/src/main/java/bbq/order/OrderRestController.java
+++ order/src/main/java/bbq/order/OrderRestController.java
@@ -2,6 +2,7 @@ package bbq.order;
 
 import bbq.order.model.Order;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,13 +13,16 @@ public class OrderRestController {
 
     private final OrderRepository orderRepository;
 
+    private final RabbitTemplate rabbitTemplate;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Order post(@RequestBody Order order) {
         // 1. Save Order
         var savedOrder = orderRepository.save(order);
 
-        // 2. TODO Publish order
+        // 2. Publish order
+        rabbitTemplate.convertAndSend("orders", "", order);
 
         // 3. Return order
         return savedOrder;
--- order/src/main/java/bbq/order/config/RabbitMqConfiguration.java
+++ order/src/main/java/bbq/order/config/RabbitMqConfiguration.java
@@ -1,5 +1,12 @@
 package bbq.order.config;
 
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Declarables;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.support.converter.Jackson2JavaTypeMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
@@ -7,16 +14,23 @@ import org.springframework.stereotype.Component;
 @Configuration
 public class RabbitMqConfiguration {
 
-    /**
-     @Bean public Jackson2JsonMessageConverter messageConverter() {
-     var messageConverter = new Jackson2JsonMessageConverter();
-     messageConverter.setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence.INFERRED);
-     return messageConverter;
+
+     @Bean
+     public Jackson2JsonMessageConverter messageConverter() {
+          var messageConverter = new Jackson2JsonMessageConverter();
+          messageConverter.setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence.INFERRED);
+          return messageConverter;
      }
 
-     @Bean public Declarables rabbitDeclarables() {
-     return new Declarables();
+     @Bean
+     public Declarables rabbitDeclarables() {
+          var exchange = new FanoutExchange("orders");
+          var queue = QueueBuilder.nonDurable("delivery.orders").build();
+          var binding = BindingBuilder.bind(queue).to(exchange);
+          return new Declarables(
+                  exchange,
+                  queue,
+                  binding
+          );
      }
-     */
-
 }
