ef55d2eeaa56bb0ebd1d0090676c1a97d2c89465
SteffenHub <steffenhub@users.noreply.github.com>
2025-02-27 03:39:52 +0100

    Added Awake Service

--- Frontend/Intern/src/main/java/com/freddys_bbq_frontend/DemoApplication.java
+++ Frontend/Intern/src/main/java/com/freddys_bbq_frontend/DemoApplication.java
@@ -2,9 +2,11 @@ package com.freddys_bbq_frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class DemoApplication {
 
     public static void main(String[] args) {
